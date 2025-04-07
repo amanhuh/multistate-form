a57332f5f73aac775202388c05b7230cbae04de3
Aman <amngupta402@gmail.com>
2025-04-07 15:40:53 +0530

    pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <form>
+    <form className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4 text-black aspec">AA</h1>
       
     </form>
   );
